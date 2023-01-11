1c983a48ca66b7ef12075ddaf685543e34ff82b2
corey <coreybatchler7@gmail.com>
2023-01-11 14:38:36 +1000

    change back to NetworkStream for more specific tests

--- Server/Packets/IPacketDeliveryService.cs
+++ Server/Packets/IPacketDeliveryService.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Moonlapse.Server.Packets {
     public interface IPacketDeliveryService {
-        public Task SendPacketAsync(Stream stream, Packet packet);
-        public Task<Packet> ReceivePacketAsync(Stream stream);
+        public Task SendPacketAsync(NetworkStream stream, Packet packet);
+        public Task<Packet> ReceivePacketAsync(NetworkStream stream);
     }
 }
